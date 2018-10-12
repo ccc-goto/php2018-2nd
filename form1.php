17937f7c040b177500a9152f041fbdf383683673
ccc-goto <goto@ccg.ac.jp>
2018-10-12 04:48:52 +0000

    sunnytizing sample

---
+++ form1.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<title>form入力</title>
+</head>
+<body>
+<form action="form2.php" method="post">
+名前は？:<input type="text" name="name">
+年齢は？:<input type="text" name="age">
+ <input type="submit">
+</form>
+</body></html>
---
+++ form2.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<title>formデータ出力</title>
+</head>
+<body>
+名前は<?php echo $_POST['name']; ?>
+年齢は<?php echo $_POST['age']; ?>
+</body></html>
